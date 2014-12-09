b2103402cae142755679e573b6db9f57d7aa5e52
Philipp Heckel <philipp.heckel@gmail.com>
2014-12-09 21:49:29 +0100

    Update for CleanupRemoteFile

--- src/main/java/org/syncany/plugins/ftp/FtpTransferManager.java
+++ src/main/java/org/syncany/plugins/ftp/FtpTransferManager.java
@@ -39,6 +39,7 @@ import org.syncany.plugins.transfer.StorageFileNotFoundException;
 import org.syncany.plugins.transfer.StorageMoveException;
 import org.syncany.plugins.transfer.TransferManager;
 import org.syncany.plugins.transfer.files.ActionRemoteFile;
+import org.syncany.plugins.transfer.files.CleanupRemoteFile;
 import org.syncany.plugins.transfer.files.DatabaseRemoteFile;
 import org.syncany.plugins.transfer.files.MultichunkRemoteFile;
 import org.syncany.plugins.transfer.files.RemoteFile;
@@ -358,7 +359,7 @@ public class FtpTransferManager extends AbstractTransferManager {
 		if (remoteFile.equals(MultichunkRemoteFile.class)) {
 			return multichunksPath;
 		}
-		else if (remoteFile.equals(DatabaseRemoteFile.class)) {
+		else if (remoteFile.equals(DatabaseRemoteFile.class) || remoteFile.equals(CleanupRemoteFile.class)) {
 			return databasesPath;
 		}
 		else if (remoteFile.equals(ActionRemoteFile.class)) {
